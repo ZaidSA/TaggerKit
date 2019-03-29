8077996927394e7091a80235179d452d2076d504
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-03-29 18:45:40 +0530

    Carthage support added.

---
+++ Example/TaggerKit-Carthage/TaggerKit_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  TaggerKit_Carthage.h
+//  TaggerKit-Carthage
+//
+//  Created by Zaid Pathan on 29/03/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaggerKit_Carthage.
+FOUNDATION_EXPORT double TaggerKit_CarthageVersionNumber;
+
+//! Project version string for TaggerKit_Carthage.
+FOUNDATION_EXPORT const unsigned char TaggerKit_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaggerKit_Carthage/PublicHeader.h>
+
+
