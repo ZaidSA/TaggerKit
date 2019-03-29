ac7573d61cb26981acf5b8f20de9690725a7ad48
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-03-29 17:56:24 +0530

    Carthage support added.

---
+++ TaggerKit-Carthage/TaggerKit-Carthage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaggerKit-Carthage
+//
+//  Created by Zaid Pathan on 29/03/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
